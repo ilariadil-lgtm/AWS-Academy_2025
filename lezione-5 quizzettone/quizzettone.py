6eb8287cced2cbe5ec408eeb8a652d7279e44e2c
Ilaria Diliberto <ilaria.dil@gmail.com>
2025-12-01 11:41:34 +0100

    first commit

--- lezione-5 quizzettone/quizzettone.py
+++ lezione-5 quizzettone/quizzettone.py
@@ -1,1 +1,28 @@
-prova
+
+
+
+
+
+
+
+def valida_scelta(scelta: str) -> bool:
+
+    scelta_tmp = scwelta.upper()
+    if scelta_tmp == "A" or scelta_tmp =="B" or scelta_tmp == "C" or scelta_tmp == "D":
+        return True
+    else:
+        return False
+    
+def mostra_domanda() -> None:
+    print( 
+        
+        """
+          
+          Chi parteciperà a Sanremo 2026?
+          
+          A. Nayt
+          B. La nina
+          C. Nilla Pizzi
+          D. Rocco Papaleo
+          
+          """)
