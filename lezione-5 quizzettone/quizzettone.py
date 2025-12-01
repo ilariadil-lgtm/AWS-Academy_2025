3c7cf4c0df9aa0f587d1365ac9985119f8142fdb
Ilaria Diliberto <ilaria.dil@gmail.com>
2025-12-01 12:03:46 +0100

    add

--- lezione-5 quizzettone/quizzettone.py
+++ lezione-5 quizzettone/quizzettone.py
@@ -31,13 +31,11 @@ def mostra_domanda() -> None:
 
 def raccogli_risposta() -> str:
         """Chiede all'utente di inserire la proprio scelta e la restituisce."""
-        
-        return input("Inserisci la tua scelta:)"
+        return input("Inserisci la tua scelta:")
                      
 def valida_scelta(scelta: str) -> bool:
-        """ Verifica se la scelta è valida (A,B,C,D) e restituisce True o False."""
-
-    scelta_tmp = scwelta.upper()
+    """ Verifica se la scelta è valida (A,B,C,D) e restituisce True o False."""
+    scelta_tmp = scelta.upper()
     if scelta_tmp == "A" or scelta_tmp =="B" or scelta_tmp == "C" or scelta_tmp == "D":
         return True
     else:
@@ -47,3 +45,5 @@ mostra_domanda()
 
 risposta_da_validare: str = raccogli_risposta()
 risposta_validata: bool= valida_scelta(risposta_da_validare)
+
+print(risposta_validata)
