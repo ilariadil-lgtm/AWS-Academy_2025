1ef3909322244fdb68bb7daa8a43707fd0076931
Ilaria Diliberto <ilaria.dil@gmail.com>
2025-12-02 11:33:23 +0100

    add fix

--- lezione-5 quizzettone/quizzettone.py
+++ lezione-5 quizzettone/quizzettone.py
@@ -26,23 +26,29 @@ mostra_feedback(messaggio) (senza return)
 - Non restituisce nulla
 """
     
-def mostra_domanda() -> None:
+def mostra_domanda(domanda: str) -> None:
     """Stampa la domanda e le opzioni di risposta."""
  
-    print(  """
-          Chi parteciperà a Sanremo 2026?
-          
-          A. Nayt
-          B. La nina
-          C. Nilla Pizzi
-          D. Rocco Papaleo
+    print( domanda)
+    
+def leggi_file() -> None:
+    with open("domande.txt", "r") as file:
+        content = file.read()
+        return content      
 
-          """)
+def estrai_index(content: str) -> int:
+    return content.index("£")
+
+def estrai_domanda(content: str, index: int) -> str:
+    return  content[0:index]
+    
+def estrai_risposta(content: str, index: int) -> str:
+    return content[index+1:]
 
 def raccogli_risposta() -> str:
         """Chiede all'utente di inserire la proprio scelta e la restituisce."""
         return input("Inserisci la tua scelta:")
-                     
+
 def valida_scelta(scelta: str) -> bool:
     """ Verifica se la scelta è valida (A,B,C,D) e restituisce True o False."""
     scelta_tmp = scelta.upper()
@@ -58,9 +64,8 @@ def genera_feedback(scelta:str, is_risposta_corretta: bool) -> str:
      else:
           return "Non hai indovinato, peccato ritenta"
      
-
-def is_risposta_esatta(scelta: str) -> bool:
-    if scelta.upper() == "A":
+def is_risposta_esatta(scelta: str, risposta_esatta: str) -> bool:
+    if scelta.upper() == risposta_esatta:
         return True
     else:
         return False
@@ -76,16 +81,22 @@ def mostra_feedback(messaggio: str) -> None:
         """)
     
 def main():
+    content: str = leggi_file()
+    index: int = estrai_index(content)
+    domanda: str = estrai_domanda(content, index)
+    risposta: str = estrai_risposta(content, index)
+
     is_risposta_corretta: bool = False
     while True:
-        mostra_domanda()
+        mostra_domanda(domanda)
         risposta_da_validare: str = raccogli_risposta()
         risposta_validata: bool = valida_scelta(risposta_da_validare)
         feedback: str = ""
 
         if risposta_validata == True:
-            is_risposta_corretta = is_risposta_esatta(risposta_da_validare)
-            feedback = genera_feedback(is_risposta_corretta)
+            print(f"sono qui: {risposta}")
+            is_risposta_corretta = is_risposta_esatta(risposta_da_validare, risposta)
+            feedback = genera_feedback(risposta_da_validare, is_risposta_corretta)
         else: 
             feedback = "Inserisci solo la risposta tra le opzioni elencate"
 
