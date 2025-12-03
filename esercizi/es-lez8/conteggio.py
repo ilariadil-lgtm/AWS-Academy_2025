faedfb6cf0920dd5042f8c2f013f4f4e08174815
Ilaria Diliberto <ilaria.dil@gmail.com>
2025-12-03 21:31:38 +0100

    add es5

---
+++ esercizi/es-lez8/conteggio.py
@@ -0,0 +1,10 @@
+voti: list[str] = ["A", "B", "A", "C", "B", "A", "D", "B", "C", "A"]
+conteggio: dict[str, int] = {}
+
+for v in voti:              # <--- itera sulla lista, non sul dizionario
+    if v in conteggio:
+        conteggio[v] += 1
+    else:
+        conteggio[v] = 1
+
+print(conteggio)            # <--- print fuori dal ciclo
