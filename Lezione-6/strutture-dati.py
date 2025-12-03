234a5a83b4eb0af5b9c111bb7f556b0747b233c1
Ilaria Diliberto <ilaria.dil@gmail.com>
2025-12-03 11:34:36 +0100

    add liste lez-8

--- Lezione-6/strutture-dati.py
+++ Lezione-6/strutture-dati.py
@@ -1,12 +1,47 @@
-stringhe: list[str] = ["Pippo"]
+"""Liste in Python"""
+"""
+stringhe: list[str | int] = ["Pippo", 10, "Paperino", 20, "Minnie"]
 
 stringhe.append("Pluto")
 stringhe.append("Topolino")
+stringhe.pop(2)
+stringhe.remove("Pippo")
 
-deleted_value = stringhe.pop()
-deleted_values: list[str] = []
+deleted_values: list[str] = []  
 
-deleted_values.append(deleted_value)
+value_to_check_and_delete: str = "Pluto"
 
+is_value_in_the_list: bool = value_to_check_and_delete in stringhe
+
+if is_value_in_the_list == True:
+        index_Value_to_delete = stringhe.index(value_to_check_and_delete)
+        deleted_value = stringhe.pop(index_Value_to_delete)
+        deleted_values.append(deleted_value)
+else:
+        print(f"{value_to_check_and_delete} Il valore non è presente nella lista {stringhe}")
+
+
+#index_Value_to_delete = stringhe.index(value_to_check_and_delete)
+
+#deleted_value = stringhe.pop(index_Value_to_delete)
+
+#deleted_values.append(deleted_value)
+
+print("*"*30)
 print(stringhe)
 print(deleted_values)
+#print(index_Value_to_delete)
+#print(is_value_in_the_list)
+
+"""
+
+
+"""Dizionari in Python"""
+
+dizionario: dict[str, int] = {
+    "Nome": "Pippo",
+    "tipo": "cane",
+    "email": "pippo@disney.com"
+}
+
+print(dizionario)
