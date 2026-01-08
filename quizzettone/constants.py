e75ef123f83e18d8b06d9c98d797ef55a829c608
Ilaria Diliberto <ilaria.dil@gmail.com>
2026-01-08 16:19:45 +0100

    refactor: creata nuova modalità di suddivisione del codice

---
+++ quizzettone/constants.py
@@ -0,0 +1,3 @@
+BASE_URL: str = "https://raw.githubusercontent.com/emanuelegurini/aws-restart/refs/heads/main/quizzettone"
+LISTA_DOMANDE_URL: str = f"{BASE_URL}/domande.txt"
+BASE_DOMANDA_SINGOLA_URL: str = f"{BASE_URL}/domande_risposte" 
